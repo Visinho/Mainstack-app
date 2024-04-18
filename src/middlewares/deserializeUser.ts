71dac65851fd1dceafc67cca9f84e2035b780038
Visinho <Elvisvisinho@rocketmail.com>
2024-04-18 10:21:37 +0100

    RefreshToken logic

--- src/middlewares/deserializeUser.ts
+++ src/middlewares/deserializeUser.ts
@@ -1,9 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 import { get } from 'lodash';
 import { verifyJwt } from "../utils/jwt.utils";
+import { reIssueAccessToken } from '../services/session.service';
 
-const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
+const deserializeUser = async (req: Request, res: Response, next: NextFunction) => {
     const accessToken = get(req, "headers.authorization", "").replace(/^Bearer\s/, "");
+
+    const refreshToken = get(req, "headers.x-refresh")
 
     if(!accessToken) {
         return next()
@@ -14,6 +17,21 @@ const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
 
     if(decoded) {
         res.locals.user = decoded;
+        return next();
+    }
+
+    if(expired && refreshToken) {
+        const newAccessToken = await reIssueAccessToken({
+            refreshToken
+        })
+
+        if(newAccessToken) {
+            res.setHeader("x-access-token", newAccessToken)
+        }
+
+        const result = verifyJwt(newAccessToken);
+
+        res.locals.user = result.decoded;
         return next();
     }
 
