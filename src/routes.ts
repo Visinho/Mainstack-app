be48bbd7ef3e693fc211ef481f5d3491192afedc
Visinho <Elvisvisinho@rocketmail.com>
2024-04-19 09:21:58 +0100

    Updating my routes file WIP

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,8 @@ import { createUserSchema } from "./schema/user.schema";
 import { createUserSessionHandler, deleteUserSessionHandler, getUserSessionHandler } from "./controllers/session.controller";
 import { createSessionSchema } from "./schema/session.schema";
 import requireUser from "./middlewares/requireUser";
+import { createProductSchema, deleteProductSchema, getProductSchema, updateProductSchema } from "./schema/product.schema";
+import { createProductHandler, deleteProductHandler, getProductHandler, updateProductHandler } from "./controllers/product.controller";
 
 function routes(app: Express) {
     app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
@@ -12,6 +14,10 @@ function routes(app: Express) {
     app.post("/api/sessions", validateResource(createSessionSchema), createUserSessionHandler);
     app.get("/api/sessions", requireUser, getUserSessionHandler);
     app.delete("/api/sessions", requireUser, deleteUserSessionHandler);
+    // app.post("/api/products", [requireUser, validateResource(createProductSchema)], createProductHandler);
+    // app.put("/api/products/update", [requireUser, validateResource(updateProductSchema)], updateProductHandler);
+    // app.get("/api/products/get", validateResource(getProductSchema), getProductHandler);
+    // app.delete("/api/products/delete", [requireUser, validateResource(deleteProductSchema)], deleteProductHandler);
     
 }
 
