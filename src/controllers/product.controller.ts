1afc76e0e103ccc3c2e8adfa495675675a5e9f1e
Visinho <Elvisvisinho@rocketmail.com>
2024-04-19 08:48:47 +0100

    get product logic

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { CreateProductInput, UpdateProductInput } from "../schema/product.schema";
-import { createProduct } from "../services/product.service";
+import { createProduct, getProduct } from "../services/product.service";
 
 export async function createProductHandler(req: Request<{}, {}, CreateProductInput["body"]>, res: Response) {
     try {
@@ -19,12 +19,33 @@ export async function createProductHandler(req: Request<{}, {}, CreateProductInp
     }
 }
 
+export async function getProductHandler(req: Request, res: Response) {
+    try {
+        const productId = req.params.productId;
+
+        // Call the service function to get the product
+        const product = await getProduct({ productId });
+
+        // If the product is not found, send a 404 response
+        if (!product) {
+            return res.sendStatus(404);
+        }
+
+        // Send the product in the response
+        return res.send(product);
+    } catch (error) {
+        // Handle errors
+        console.error('Error fetching product:', error);
+        return res.status(500).send('Internal Server Error');
+    }
+}
+
+
 export async function updateProductHandler(req: Request<UpdateProductInput["params"]>, res: Response) {
 
 }
-export async function getProductHandler(req: Request, res: Response) {
 
-}
+
 export async function deleteProductHandler(req: Request, res: Response) {
 
 }
