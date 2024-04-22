0fcf4fef10572f79eb06ecdfb91b53f5ed2f521c
Visinho <Elvisvisinho@rocketmail.com>
2024-04-22 08:27:41 +0100

    Unit testing

--- src/__tests__/product.test.ts
+++ src/__tests__/product.test.ts
@@ -1,82 +1,95 @@
-// import supertest from "supertest";
-// import createServer from "../utils/server";
-// import { createProduct } from "../services/product.service";
-// import mongoose from "mongoose";
-// import { signJwt } from "../utils/jwt.utils";
+import supertest from "supertest";
+import { createProduct } from "../services/product.service";
+import mongoose from "mongoose";
+import { signJwt } from "../utils/jwt.utils";
+import express, { Express } from 'express';
 
-// const app = createServer()
+const app: Express = express();
 
-// const userId = new mongoose.Types.ObjectId().toString();
 
-// export const productPayload = {
-// user: userId, 
-// "title": "Testing my endpoint and making sure that everything is alright",
-// "description": "After having spent hours working on this project, it is safe to say that I am finally getting to the end of it. Even though it was not an easy task, I have worked tireless and I really hope that it will work as intended so I can get reward for my hard work!",
-// "price": 300.00,
-// "image": "https://visinho/Qerundjfr.jpg"
-// }
+const userId = new mongoose.Types.ObjectId().toString();
 
-// export const userPayload = {
-//     _id: userId,
-//     email: "Elvis@gmail.com",
-//     name: "Visinho"
-// }
+export const productPayload = {
+user: userId, 
+"title": "Testing my endpoint and making sure that everything is alright",
+"description": "After having spent hours working on this project, it is safe to say that I am finally getting to the end of it. Even though it was not an easy task, I have worked tireless and I really hope that it will work as intended so I can get reward for my hard work!",
+"price": 300.00,
+"image": "https://visinho/Qerundjfr.jpg"
+}
 
-// describe("product", () => {
-//     describe("Get Product Route", () => {
-//         describe("given the product does not exist", () => {
-//             it("should return a 404", async () => {
-//                 const productId = "product-123"
+export const userPayload = {
+    _id: userId,
+    email: "Elvis@gmail.com",
+    name: "Visinho"
+}
 
-//                 await supertest(app).get(`/api/products/${productId}`).expect(404)
-//             })
-//         });
+describe("product", () => {
+    describe("Get Product Route", () => {
+        describe("given the product does not exist", () => {
+            it("should return a 404", async () => {
+                const productId = "product-123"
 
-//         // describe("given the product does exist", () => {
-//         //     it("should return a 200 status and the product", async () => {
+                await supertest(app).get(`/api/products/${productId}`).expect(404)
+            })
+        });
 
-//         //         const product = await createProduct(productPayload)
+        describe("given the product does exist", () => {
+            it("should return a 200 status and the product", async () => {
 
-//         //         const {body, statusCode} = await supertest(app).get(`/api/products/${product.productId}`)
+                const product = await createProduct(productPayload)
 
-//         //         expect(statusCode).toBe(200)
+                const {body, statusCode} = await supertest(app).get(`/api/products/${product._id}`)
 
-//         //         expect(body.productId).toBe(product.productId)
-//         //     })
-//         // });
+                expect(statusCode).toBe(200)
 
-//         describe("given the product does exist", () => {
-//             it("should return a 200 status and the product", async () => {
-//                 const product = await createProduct(productPayload);
+                expect(body.productId).toBe(product._id)
+            })
+        });
+
+        describe("given the product does exist", () => {
+            it("should return a 200 status and the product", async () => {
+                const product = await createProduct(productPayload);
         
-//                 const { body, statusCode } = await supertest(app).get(`/api/products/${product.productId}`);
+                const { body, statusCode } = await supertest(app).get(`/api/products/${product._id}`);
         
-//                 expect(statusCode).toBe(200);
-//                 expect(body.productId).toBe(product.productId);
-//             }, 20000); // Timeout set to 10 seconds
-//         });
+                expect(statusCode).toBe(200);
+                expect(body.productId).toBe(product._id);
+            }, 20000); // Timeout set to 10 seconds
+        });
         
 
-//     });
+    });
 
-//     describe("create product route", () => {
-//         describe("given the user is not logged in", () => {
-//             it("should return a 403", async () => {
-//                 const {statusCode} = await supertest(app).post("/api/products");
+    describe("create product route", () => {
+        describe("given the user is not logged in", () => {
+            it("should return a 403", async () => {
+                const {statusCode} = await supertest(app).post("/api/products");
 
-//                 expect(statusCode).toBe(403);
-//             });
-//         });
+                expect(statusCode).toBe(403);
+            });
+        });
 
-//         describe("given the user is logged in", () => {
-//             it("should return a 200 and create the product", async () => {
-//                 const jwt = signJwt(userPayload)
-//                 const {statusCode, body} = await supertest(app).post("/api/products").set("Authorization", `Bearer ${jwt}`).send(productPayload)
+        describe("given the user is logged in", () => {
+            it("should return a 200 and create the product", async () => {
+                const jwt = signJwt(userPayload)
+                const {statusCode, body} = await supertest(app).post("/api/products").set("Authorization", `Bearer ${jwt}`).send(productPayload)
 
-//                 expect(statusCode).toBe(200)
-//                 expect(body).toEqual({})
+                expect(statusCode).toBe(200)
+                expect(body).toEqual(
+                    {
+                        user: expect.any(String),
+                        title: "Testing my test app to be sure nothing broke and making sure that everything is alright",
+                        description: "After having spent hours working on this project, it is safe to say that I am finally getting to the end of it. Even though it was not an easy task, I have worked tireless and I really hope that it will work as intended so I can get reward for my hard work!",
+                        price: 300,
+                        image: "https://visinho/Qerundjfr.jpg",
+                        _id: expect.any(String),
+                        productId: expect.any(String),
+                        createdAt: "2024-04-21T18:29:31.792Z",
+                        updatedAt: expect.any(String)
+                    
+                })
 
-//             })
-//         })
-//     })
-// });
+            })
+        })
+    })
+});
---
+++ src/__tests__/user.test.ts
@@ -0,0 +1,121 @@
+import * as Userservice from "../services/user.service";
+import mongoose from "mongoose";
+import supertest from "supertest";
+import express, { Express } from 'express';
+import * as SessionService from "../services/session.service";
+import { createUserSessionHandler } from "../controllers/session.controller";
+
+const app: Express = express();
+
+const userId = new mongoose.Types.ObjectId().toString();
+
+const userInput = {
+    email: "Visinho@gmail.com",
+    name: "Jane Doe",
+    password: "Password123",
+    passwordConfirmation: "Password123"
+}
+
+const userPayload = {
+    email: "Visinho@gmail.com",
+    name: "Jane Doe",
+    _id: userId,
+}
+
+const sessionPayload = {
+    _id: new mongoose.Types.ObjectId().toString(),
+    user: userId,
+    valid: true,
+    userAgent: "PostmanRuntime/7.36.0",
+    createdAt: new Date("2024-04-18T01:14:43.473+00:00"),
+    updatedAt: new Date("2024-04-18T01:14:48.473+00:00")
+}
+
+describe("user", () => {
+    // User Registration
+    describe("User registration", () => {
+        describe("Given the username and password are valid", () => {
+            it("should return the user payload", async () => {
+                const createUserServiceMock = jest.spyOn(Userservice, "createUser")
+                //@ts-ignore
+                .mockReturnValueOnce(userPayload);
+
+                const {statusCode, body} = await supertest(app).post("/api/users").send(userInput);
+
+                expect(statusCode).toEqual(200);
+
+                expect(body).toEqual(userPayload);
+
+                expect(createUserServiceMock).toHaveBeenCalledWith(userInput);
+            })
+        })
+
+        describe("Given the passwords do not match", () => {
+            it("should return a 400", async () => {
+                const createUserServiceMock = jest.spyOn(Userservice, "createUser")
+                //@ts-ignore
+                .mockReturnValueOnce(userPayload);
+
+                const {statusCode, body} = await supertest(app).post("/api/users").send({...userInput, passwordConfirmation: "Doesnotmatch"});
+
+                expect(statusCode).toBe(400);
+
+                expect(createUserServiceMock).not.toHaveBeenCalledWith();
+            })
+
+        })
+
+        describe("Given the user service throws", () => {
+            it("should return a 409 error", async () => {
+                const createUserServiceMock = jest.spyOn(Userservice, "createUser")
+                //@ts-ignore
+                .mockRejectedValue("Error 409");
+
+                const {statusCode, body} = await supertest(app).post("/api/users").send(userInput);
+
+                expect(statusCode).toEqual(409);
+
+                expect(createUserServiceMock).toHaveBeenCalled();
+            })
+        })
+    })
+
+    describe("Create user session", () => {
+        describe("Given the username and password are valid", async () => {
+            it("should return a signed accessToken and refreshToken", async () => {
+                jest.spyOn(Userservice, "validatePassword").
+                //@ts-ignore
+                mockReturnValueOnce(userPayload)
+
+                jest.spyOn(SessionService, "createSession").
+                //@ts-ignore
+                mockReturnValueOnce(sessionPayload)
+
+                const req = {
+                    get: () => {
+                     return "a user agent"  
+                    },
+                    body: {
+                        email: "Test@gmail.com",
+                        password: "testing123"
+                    }
+                }
+
+                const send = jest.fn();
+
+                const res = {
+                    send
+                }
+
+                // @ts-ignore
+            await createUserSessionHandler(req, res)
+
+            expect(send).toHaveBeenCalledWith({
+                accessToken: expect.any(String), 
+                refreshToken: expect.any(String)
+            })
+            })
+        })
+    })
+
+})
